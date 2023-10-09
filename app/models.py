c5ced871aac264b83dc47a432c957291e055b4e8
David Jachym <jachym19@gmail.com>
2023-10-08 19:38:22 -0500

    update

---
+++ app/models.py
@@ -0,0 +1,31 @@
+from app import db
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(20), index=True)
+    last_name = db.Column(db.String(50), index=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(120))
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, first_name, last_name, email, password, date_created):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.date_created = date_created
+
+class CartItems(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    item_name = db.Column(db.String(100), index=True)
+    category = db.Column(db.String(50), index=True)
+    price = db.Column(db.Integer, index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, item_name, category, price, user_id):
+        self.item_name = item_name
+        self.category = category
+        self.price = price
+        self.user_id = user_id
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,49 @@
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash, session
-from .forms import PokemonForm, SignUpForm, LogInForm, EditProfileForm, CatchPokemonForm
+from .forms import SignUpForm, LoginForm, EditProfileForm
 import requests as r
-from .models import User, CatchPokemon
+from flask_bcrypt import Bcrypt
+# from .models import User, CatchPokemon
 from flask_login import login_user, logout_user, current_user, login_required
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    url = "https://fakestoreapi.com/products"
+    response = r.get(url)
+    if response.ok:
+        products = response.json()
+        
+    return render_template("index.html", products=products)
+
+@app.route("/sign-up", methods=['GET', 'POST'])
+def signup():
+    form = SignUpForm()
+    if request.method == 'POST':
+        if form.validate():
+            first_name = form.first_name.data.title()
+            last_name = form.last_name.data.title()
+            email = form.email.data
+            password = form.password.data
+
+            user_exists = User.query.filter_by(email=email).first()
+
+            if user_exists:
+                flash("A user with that email already exists. Please use a different email.", "danger")
+                return redirect(url_for("sign-up"))
+            else:
+                hashed_password = Bcrypt.generate_password_hash(password).decode('utf-8')
+                new_user = User(first_name=first_name, last_name=last_name, email=email, password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+
+                flash('Account created successfully!', 'success')
+                return redirect(url_for('login'))
+        
+        else:
+            flash('Passwords do not match. Please try again.', 'danger')
+            return redirect(url_for('signup'))
+    
+    return render_template('signup.html', form=form)
+
+
